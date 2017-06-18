3b516ac526895dd1509077992d452cad9ffb66a9
Gaston Caruso <gstn.caruso@gmail.com>
2017-06-18 19:41:42 -0300

    WelcomeController busca la imágen según el id que recibe como parámetro en la url

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,17 @@
 class WelcomeController < ApplicationController
   def index
+    @image_url = find_image_from_url.image_url
+  end
+
+  private
+
+  def welcome_params
+    params.require(:image_id)
+  end
+
+  def find_image_from_url
+    Image.find(params[:image_id])
+  rescue ActiveRecord::RecordNotFound
+    Image.last
   end
 end
