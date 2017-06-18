b7922db28254a8caaa3a461dcd08e49d9cde109c
Gaston Caruso <gstn.caruso@gmail.com>
2017-06-18 20:17:07 -0300

    Arreglo test de WelcomeController#index

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 describe WelcomeController, type: :controller do
 
+  # TODO: Replace this with FactoryGirl
+  before do
+    Image.create!(name: 'example_image',
+                  image: File.open("#{Rails.root}/spec/fixtures/files/sapo.jpg"))
+  end
+
   describe 'GET #index' do
-    it('returns http success') do
-      get :index
+    it('when an image id is passed returns http success') do
+      get :index, params: { image_id: '1' }
       expect(response).to have_http_status(:success)
     end
   end
-
 end
