800908cb377db21aea46ca61690e01e59605dd18
Gaston Caruso <gstn.caruso@gmail.com>
2017-06-23 00:00:09 -0300

    Incluyo Cloudinary en ImageUploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,11 @@
 class ImageUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
   storage :file
+
+  include Cloudinary::CarrierWave
+
+  process convert: 'png'
+  process tags: ['post_picture']
+
+  version(:standard) { process resize_to_fill: [100, 150, :north] }
+  version(:thumbnail) { resize_to_fit(50, 50) }
 end
