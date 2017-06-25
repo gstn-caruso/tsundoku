95322302d08d332941752034390f8ccb70a9c103
Gaston Caruso <gstn.caruso@gmail.com>
2017-06-25 18:47:11 -0300

    Refactor ImagesController

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,7 @@
-
 class ImagesController < ApplicationController
-  
+
+  SAVE_QR_REMINDER = 'Print the qr code or download it to use it later'
+
   def index
     @images = Image.all
   end
@@ -15,12 +16,13 @@ class ImagesController < ApplicationController
   
   def create
     @image = Image.new image_params
-    
-    if @image.save
-      image_path = "public/qr_codes/#{@image.id}.png"
-      Qr4r::encode(@image.id.to_s, image_path, :pixel_size => 10)
+    image_id = @image.id.to_s
 
-      redirect_to "/qr/#{@image.id.to_s}", notice: 'Print the qr code or download it to use it later'
+    if @image.save
+      image_path = "public/qr_codes/#{image_id}.png"
+      Qr4r::encode(image_id, image_path, pixel_size: 10)
+
+      redirect_to "/qr/#{image_id}", notice: SAVE_QR_REMINDER
     else
       render :new
     end
@@ -31,7 +33,8 @@ class ImagesController < ApplicationController
   end
 
   private
-    def image_params
-      params.require(:image).permit(:name, :image)
-    end
+
+  def image_params
+    params.require(:image).permit(:name, :image)
+  end
 end
