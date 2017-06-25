dcb0b5e10c6e0be0a8c22aaa3046428bf422fe90
agustin <garciasmithagustin@gmail.com>
2017-06-25 14:12:29 -0300

    add message to qr view

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -17,17 +17,17 @@ class ImagesController < ApplicationController
     @image = Image.new image_params
     
     if @image.save
-      image_path = "public/uploads/qr/#{@image.id}.png"
+      image_path = "public/uploads/qr_code/#{@image.id}.png"
       Qr4r::encode(@image.id.to_s, image_path, :pixel_size => 10)
 
-      redirect_to 'qr', id:@image.id, notice: 'Print the qr code or download it to use it later'
+      redirect_to "/qr/#{@image.id.to_s}", notice: 'Print the qr code or download it to use it later'
     else
       render :new
     end
   end
 
   def qr
-    @qr_url = root_path + "uploads/qr_code/#{params[:id]}.png"
+    @qr_url = root_url + "uploads/qr_code/#{params[:id]}.png"
   end
 
   private
