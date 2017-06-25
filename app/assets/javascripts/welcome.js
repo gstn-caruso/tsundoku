3f565d95968bc5b65cf0a051d48b2be3315b151b
agustin <garciasmithagustin@gmail.com>
2017-06-25 20:28:16 -0300

    create new view for qr scan and move js to it

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -5,34 +5,3 @@
 //= require aframe-ar.min
 //= require three.min
 //= require instascan.min
-
-$(document).ready(function () {
-    $('#reader').html5_qrcode(function(data){
-            alert(data)
-        },
-        function(error){
-            console.log(error)
-        }, function(videoError){
-            console.log(videoError)
-        }
-    );
-});
-
-$(document).ready(function () {
-    var preview = document.getElementById('preview');
-    if(preview){
-        var scanner = new Instascan.Scanner({ video: preview });
-        scanner.addListener('scan', function (content) {
-            alert(content);
-        });
-        Instascan.Camera.getCameras().then(function (cameras) {
-            if (cameras.length > 0) {
-                scanner.start(cameras[0]);
-            } else {
-                console.error('No cameras found.');
-            }
-        }).catch(function (e) {
-            console.error(e);
-        });
-    }
-});
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index/:image_id' => 'welcome#index'
-  root 'welcome#index'
+  get 'qr_reader' => 'welcome#qr_reader'
+  root 'welcome#qr_reader'
 
   resources :images, only: %i[index create new show]
   get 'images', to: :index, controller: 'images'
   get 'qr/:id' => 'images#qr'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
