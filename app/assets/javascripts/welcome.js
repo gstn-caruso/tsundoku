6b97617c6eace24d0a2b52a1a09b77fb713b513e
agustin <garciasmithagustin@gmail.com>
2017-06-25 17:46:09 -0300

    change js library

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,7 +4,7 @@
 //= require aframe.min
 //= require aframe-ar.min
 //= require three.min
-//= require html5-qrcode.min
+//= require instascan.min
 
 $(document).ready(function () {
     $('#reader').html5_qrcode(function(data){
@@ -16,4 +16,23 @@ $(document).ready(function () {
             console.log(videoError)
         }
     );
+});
+
+$(document).ready(function () {
+    var preview = document.getElementById('preview');
+    if(preview){
+        var scanner = new Instascan.Scanner({ video: preview });
+        scanner.addListener('scan', function (content) {
+            alert(content);
+        });
+        Instascan.Camera.getCameras().then(function (cameras) {
+            if (cameras.length > 0) {
+                scanner.start(cameras[0]);
+            } else {
+                console.error('No cameras found.');
+            }
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
 });
