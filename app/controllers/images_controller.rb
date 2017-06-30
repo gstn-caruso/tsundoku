d0666e9fc32298f0b479a6fdc5ca7c4a5d1ef132
agustin <garciasmithagustin@gmail.com>
2017-06-30 20:34:00 -0300

    add qr and ar image together

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,3 +1,5 @@
+require 'RMagick'
+
 class ImagesController < ApplicationController
 
   SAVE_QR_REMINDER = 'Print the qr code or download it to use it later'
@@ -18,8 +20,7 @@ class ImagesController < ApplicationController
     @image = Image.new image_params
 
     if @image.save
-      image_path = "public/qr_codes/#{@image.id}.png"
-      Qr4r::encode(@image.id.to_s, image_path, pixel_size: 10)
+      generate_qr_ar_image(@image.id)
 
       redirect_to "/qr/#{@image.id}", notice: SAVE_QR_REMINDER
     else
@@ -27,8 +28,24 @@ class ImagesController < ApplicationController
     end
   end
 
+  def generate_qr_ar_image(image_id)
+    image_path = "public/qr_codes/#{image_id}.png"
+    Qr4r::encode(@image.id.to_s, image_path, pixel_size: 20)
+
+    #this will be the final image
+    big_image = Magick::ImageList.new
+
+    image_row = Magick::ImageList.new
+    image_row.push(Magick::Image.read("public/qr_codes/#{@image.id}.png").first)
+    image_row.push(Magick::Image.read('app/assets/images/hiro.png').first)
+
+    big_image.push(image_row.append(false))
+
+    big_image.append(true).write("public/qr_codes/hiro#{image_id}.png")
+  end
+
   def qr
-    @qr_url = root_url + "qr_codes/#{params[:id]}.png"
+    @qr_url = root_url + "qr_codes/hiro#{params[:id]}.png"
   end
 
   private
